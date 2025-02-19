dd19fd5c47ec71e31bbc6bef5318d1dafc44c8af
uyen2910 <21T1020099@husc.edu.vn>
2025-02-19 14:56:44 +0700

    test non blocking

--- TEAM_15/DuongThiThuUyen/ESP32_LED_Blink/src/main.cpp
+++ TEAM_15/DuongThiThuUyen/ESP32_LED_Blink/src/main.cpp
@@ -1,29 +1,61 @@
 #include <Arduino.h>
-#define LED_PIN 23
+#define LED_PIN 23  // Chọn chân GPIO 2 để điều khiển LED
 
-//Khởi tạo, chạy 1 lần duy nhất cho đến khi hết pin
+ulong currentMiliseconds = 0; // thời gian hiện tại ms
+ulong ledStart = 0;           // thời gian on/off LED
+
+void Use_Blocking();
+bool IsReady(ulong &ulTimer, uint32_t milisecond);
+void Use_Non_Blocking();
+
 void setup() {
-  // put your setup code here, to run once:
-  //cau lenh mac dinh de chay duoc ham print ben duoi
-  // Serial.begin(115200);
-  // Serial.println("Hello IOT");
-
-  pinMode(LED_PIN, OUTPUT);
-
+  Serial.begin(115200);     //Khởi tạo Serial
+  pinMode(LED_PIN, OUTPUT); // Cấu hình chân LED_PIN là đầu ra
 }
 
-//Lặp chạy mãi mãi
 void loop() {
-  // put your main code here, to run repeatedly:
-  // static int i = 0;
-  // ulong t = millis();
-  // Serial.print(t);
-  // Serial.print(" Loop time: ");
-  // Serial.println(i++);
-  // delay(1000);
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+  currentMiliseconds = millis();  //nhận thời gian hiện tại tính bằng msms
+  //Use_Blocking();         //blocking
+  Use_Non_Blocking();       //Non-blocking
+
+  Serial.print("Timer :");
+  Serial.println(currentMiliseconds);
+
 }
 
+void Use_Blocking()
+{
+  digitalWrite(LED_PIN, HIGH); // Bật LED
+  Serial.println("LED -> ON");
+  delay(1000);               // Đợi 1 giây
+  digitalWrite(LED_PIN, LOW); // Tắt LED
+  Serial.println("LED -> OFF");
+  delay(1000); // Đợi 1 giây
+}
+
+bool IsReady(ulong &ulTimer, uint32_t milisecond)
+{
+  if (currentMiliseconds - ulTimer < milisecond)
+    return false;
+  ulTimer = currentMiliseconds;
+  return true;
+}
+
+void Use_Non_Blocking()
+{
+  static bool isLED_ON = false;
+  if (!IsReady(ledStart, 1000))
+    return;
+
+  if (!isLED_ON)
+  {
+    digitalWrite(LED_PIN, HIGH); // Bật LED
+    Serial.println("NonBlocking LED -> ON");
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW); // Tắt LED
+    Serial.println("NonBlocking LED -> OFF");
+  }
+  isLED_ON = !isLED_ON;
+}
