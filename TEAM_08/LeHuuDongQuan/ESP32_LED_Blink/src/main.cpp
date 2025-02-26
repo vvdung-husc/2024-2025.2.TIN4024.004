848a0de403590a6ad31c0ef044710ec25f8d508d
dongwave <21T1020614@husc.edu.vn>
2025-02-26 13:46:30 +0700

    capnhatBT1

--- TEAM_08/LeHuuDongQuan/ESP32_LED_Blink/src/main.cpp
+++ TEAM_08/LeHuuDongQuan/ESP32_LED_Blink/src/main.cpp
@@ -1,21 +1,28 @@
 #include <Arduino.h>
 #define LED_PIN 23  // Chọn chân GPIO 2 để điều khiển LED
+
 ulong currentMiliseconds = 0; // thời gian hiện tại ms
 ulong ledStart = 0;           // thời gian on/off LED
+
 void Use_Blocking();
 bool IsReady(ulong &ulTimer, uint32_t milisecond);
 void Use_Non_Blocking();
+
 void setup() {
   Serial.begin(115200);     //Khởi tạo Serial
   pinMode(LED_PIN, OUTPUT); // Cấu hình chân LED_PIN là đầu ra
 }
+
 void loop() {
   currentMiliseconds = millis();  //nhận thời gian hiện tại tính bằng msms
   //Use_Blocking();         //blocking
   Use_Non_Blocking();       //Non-blocking
+
   Serial.print("Timer :");
   Serial.println(currentMiliseconds);
+
 }
+
 void Use_Blocking()
 {
   digitalWrite(LED_PIN, HIGH); // Bật LED
@@ -25,6 +32,7 @@ void Use_Blocking()
   Serial.println("LED -> OFF");
   delay(1000); // Đợi 1 giây
 }
+
 bool IsReady(ulong &ulTimer, uint32_t milisecond)
 {
   if (currentMiliseconds - ulTimer < milisecond)
@@ -32,11 +40,13 @@ bool IsReady(ulong &ulTimer, uint32_t milisecond)
   ulTimer = currentMiliseconds;
   return true;
 }
+
 void Use_Non_Blocking()
 {
   static bool isLED_ON = false;
   if (!IsReady(ledStart, 1000))
     return;
+
   if (!isLED_ON)
   {
     digitalWrite(LED_PIN, HIGH); // Bật LED
